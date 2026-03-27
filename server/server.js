d30ab58ebb391c4ae195122001851e25bff2a8aa
Ayush Raj <ayushraj.kanpur2004@gmail.com>
2026-03-28 04:05:28 +0530

    setup the db

--- server/server.js
+++ server/server.js
@@ -1,7 +1,28 @@
-require('dotenv').config();
-const express=require ('express');
+// require('dotenv').config();
+// const express=require ('express');
+// const app=express();
+// const connectDB = require('./config/db');
+
+// connectDB();
+// app.listen(process.env.PORT,()=>{
+//     console.log(`Server is running on port ${process.env.PORT}`);
+// });
+
+
+
+
+
+
+require('dotenv').config()
+const express=require('express');
+const connectDB = require('./config/db');
 const app=express();
 
-app.listen(process.env.PORT,()=>{
-    console.log(`Server is running on port ${process.env.PORT}`);
-});
+const startServer=async()=>{
+    await connectDB();
+    app.listen(process.env.PORT,()=>{
+        console.log(`Server is running on port ${process.env.PORT}`);
+    })
+}
+
+startServer();
