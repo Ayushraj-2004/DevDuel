58b42720b2f5765657600547befc40f2bf44a48b
Ayush Raj <ayushraj.kanpur2004@gmail.com>
2026-03-29 03:23:41 +0530

    created model for User

---
+++ server/models/User.js
@@ -0,0 +1,31 @@
+const mongoose=require('mongoose');
+
+const userSchema = new mongoose.Schema({
+
+    username:{
+        type: String,
+        required: true,
+        unique:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    wins:{
+        type:Number,
+        default:0
+    },
+    loss:{
+        type:Number,
+        default:0
+    },
+    rating: { type: Number, default: 0 }
+})
+
+const User=mongoose.model('User',userSchema);
+module.exports=User;
--- server/server.js
+++ server/server.js
@@ -1,18 +1,3 @@
-// require('dotenv').config();
-// const express=require ('express');
-// const app=express();
-// const connectDB = require('./config/db');
-
-// connectDB();
-// app.listen(process.env.PORT,()=>{
-//     console.log(`Server is running on port ${process.env.PORT}`);
-// });
-
-
-
-
-
-
 require('dotenv').config()
 const express=require('express');
 const connectDB = require('./config/db');
