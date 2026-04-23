1cbb00b99d4aa2eabd16032e9e3fc64407f1d54c
Ayush Raj <ayushraj.kanpur2004@gmail.com>
2026-04-23 15:34:15 +0530

    completed userProfile function

--- server/server.js
+++ server/server.js
@@ -3,8 +3,10 @@ const express=require('express');
 const connectDB = require('./config/db');
 const app=express();
 const authRoutes = require('./routes/auth');
+const userRoutes=require('./routes/user');
 app.use(express.json());
 app.use('/api/auth',authRoutes);
+app.use('/api/user',userRoutes);
 
 const startServer=async()=>{
     await connectDB();
